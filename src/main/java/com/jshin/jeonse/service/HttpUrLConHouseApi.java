4f050bdb8e9fa837da1acad885f044e2fd06d7a9
shinjooyoung <sjy9568@gmail.com>
2022-04-07 18:36:41 +0900

    공동주택가격 api 적용 및 테스트

--- src/main/java/com/jshin/jeonse/service/HttpUrLConHouseApi.java
+++ src/main/java/com/jshin/jeonse/service/HttpUrLConHouseApi.java
@@ -17,8 +17,6 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.io.BufferedReader;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -100,45 +98,4 @@ public class HttpUrLConHouseApi implements HouseApi{
         }
         return null;
     }
-
-    public Map<String,Object> requestToHouseInfo2(String pnu, String dong, String ho) throws Exception  {
-
-        Map<String, Object> resultMap = new HashMap<>();
-
-        String envUrl = env.getProperty("url");
-        String key = env.getProperty("key");
-
-        log.debug("[HTTP API] url={}, key={}", envUrl, key);
-
-        StringBuilder urlBuilder = new StringBuilder(envUrl); /*URL*/
-        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "="+key); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("pnu","UTF-8") + "=" + URLEncoder.encode(pnu, "UTF-8"));
-        urlBuilder.append("&" + URLEncoder.encode("format","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8"));
-        urlBuilder.append("&" + URLEncoder.encode("dongNm","UTF-8") + "=" + URLEncoder.encode(dong, "UTF-8"));
-        urlBuilder.append("&" + URLEncoder.encode("hoNm","UTF-8") + "=" + URLEncoder.encode(ho, "UTF-8"));
-
-        URL url = new URL(urlBuilder.toString());
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Content-type", "application/json;charset=UTF-8");
-        System.out.println("Response code: " + conn.getResponseCode());
-        BufferedReader rd;
-
-        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-            rd = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-        } else {
-            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream(), "UTF-8"));
-        }
-        StringBuilder sb = new StringBuilder();
-        String line;
-        while ((line = rd.readLine()) != null) {
-            sb.append(line);
-        }
-        ObjectMapper mapper = new ObjectMapper();
-
-        rd.close();
-        conn.disconnect();
-        return convertJsonStrToMap(sb.toString());
-    }
-
 }
